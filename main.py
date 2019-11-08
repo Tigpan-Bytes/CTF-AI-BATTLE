e096149936aeba4aa27968970e7a951a9b18a06a
BuildTools <unconfigured@null.spigotmc.org>
2019-11-07 21:39:35 -0600

    more world gen

--- main.py
+++ main.py
@@ -17,42 +17,53 @@ WHITE = (255, 255, 255)
 BOARD = (200, 200, 200)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+GREEN = (0, 90, 160)
 
 TIMEOUT = 0.1
 X_SIZE = 60
 Y_SIZE = 60
 
+
 class TimeoutException(Exception):
     def __init__(self, msg=''):
         self.msg = msg
-        
-class Bot():
+
+
+class Tile:
+    def __init__(self, walkable):
+        self.walkable = walkable
+        self.hive = False
+
+
+class Bot:
     def __init__(self, name, ai):
         self.name = name
         self.ai = ai
         self.position = (random.randrange(0, 20), random.randrange(0, 20))
 
-class Board():
+
+class Board:
     def __init__(self, w, h, bots):
         self.grid = create_grid(w, h)
-        self.resize_board(600, 600)
+        self.resize_board(math.floor(600 * max(X_SIZE / Y_SIZE, 1)), math.floor(600 * max(Y_SIZE / X_SIZE, 1)))
         self.bots = bots
-        
+
     def resize_board(self, w, h):
         self.screen = pygame.display.set_mode((w, h), pygame.RESIZABLE)
         self.cell_size = h / Y_SIZE
         self.x_plus = (w - (self.cell_size * X_SIZE)) / 2
-        
+
     def render(self):
         self.screen.fill(WHITE)
-        pygame.draw.rect(self.screen, BOARD, (self.x_plus, 0, X_SIZE * self.cell_size, Y_SIZE * self.cell_size))
-        
+        pygame.draw.rect(self.screen, BLACK, (self.x_plus, 0, X_SIZE * self.cell_size, Y_SIZE * self.cell_size))
+
         for x in range(X_SIZE):
             for y in range(Y_SIZE):
-                if not self.grid[x][y]:
-                    pygame.draw.rect(self.screen, BLACK, (x * self.cell_size + self.x_plus, y * self.cell_size,
-                                                          self.cell_size + 1, self.cell_size + 1))
-                    
+                if self.grid[x][y].walkable:
+                    pygame.draw.rect(self.screen, GREEN if self.grid[x][y].hive else BOARD,
+                                     (x * self.cell_size + self.x_plus, y * self.cell_size,
+                                      self.cell_size + 1, self.cell_size + 1))
+
     def do_bots(self):
         bot_length = len(self.bots)
         i = 0
@@ -61,24 +72,25 @@ class Board():
                 with timeout_limit():
                     self.bots[i].position = self.bots[i].ai.do_turn(self.bots[i].position)
             except TimeoutException as e:
-                print('Bot index [' + str(i) + '] ('+self.bots[i].name+') exceeded timelimit, no actions taken.')
+                print('Bot index [' + str(i) + '] (' + self.bots[i].name + ') exceeded timelimit, no actions taken.')
             except Exception as e:
-                print('Bot index [' + str(i) + '] ('+self.bots[i].name+') did a naughty. Terminating it.')
-                
+                print('Bot index [' + str(i) + '] (' + self.bots[i].name + ') did a naughty. Terminating it.')
+
                 self.bots.pop(i)
 
                 i = i - 1
                 bot_length = bot_length - 1
-                
+
             pygame.draw.rect(self.screen, RED, (self.bots[i].position[0] * self.cell_size + self.x_plus,
                                                 self.bots[i].position[1] * self.cell_size,
-                                                self.cell_size - 1, self.cell_size - 1))
-            
+                                                self.cell_size, self.cell_size))
+
             i = i + 1
-                    
+
     def update(self):
         self.render()
         self.do_bots()
+
 
 @contextmanager
 def timeout_limit(seconds=TIMEOUT):
@@ -92,84 +104,133 @@ def timeout_limit(seconds=TIMEOUT):
         # if the action ends in specified time, timer is canceled
         timer.cancel()
 
-def insert_grid(grid, quarter, w, h, x_index, y_index):
-    for x in range(math.floor(w / 2)):
-        for y in range(math.floor(h / 2)):
-            grid[(x_index + x) % w][(y_index + y) % h] = quarter[x][y]
+
+def insert_grid(grid, part, x_portion, y_portion, w, h, x_index, y_index):
+    for x in range(math.floor(w / x_portion)):
+        for y in range(math.floor(h / y_portion)):
+            grid[(x_index + x + w) % w][(y_index + y + h) % h] = part[x][y]
     return grid
+
 
 def create_grid(w, h):
-    grid_template = [[0 for y in range(math.floor(h / 2))] for x in range(math.floor(w / 2))]
-    grid_quarter_pattern = [[3,3,3,3,3,3,3,3,3,3,3,3],
-                            [0,2,0,1,3,0,1,2,1,0,0,3],
-                            [2,1,2,2,3,1,2,1,2,1,1,3],
-                            [2,0,1,1,1,2,3,0,1,2,2,3],
-                            [0,3,0,1,2,0,3,3,0,0,0,3],
-                            [3,3,3,0,1,2,1,3,3,0,0,3],
-                            [3,0,0,3,3,1,2,1,0,3,3,3],
-                            [3,0,0,0,3,3,0,2,1,0,3,0],
-                            [3,5,2,1,0,3,2,1,1,1,0,2],
-                            [3,1,1,2,1,2,1,3,2,2,1,2],
-                            [3,0,0,1,2,1,0,3,1,0,2,0],
-                            [3,3,3,3,3,3,3,3,3,3,3,3]]
+    x_portion = 2
+    y_portion = 2
+    grid_partial_pattern = [[3, 3, 3, 3, 0, 2, 1, 1, 3, 3, 3, 3],
+                            [0, 2, 0, 1, 3, 1, 1, 2, 1, 0, 0, 3],
+                            [2, 1, 2, 1, 3, 1, 2, 1, 2, 1, 1, 3],
+                            [2, 0, 2, 1, 1, 2, 0, 3, 1, 'B', 1, 3],
+                            [0, 3, 0, 1, 2, 0, 3, 3, 0, 1, 1, 3],
+                            [3, 3, 3, 0, 1, 2, 1, 3, 3, 3, 1, 3],
+                            [3, 1, 3, 3, 3, 1, 2, 1, 0, 3, 3, 3],
+                            [3, 1, 1, 0, 3, 3, 0, 2, 1, 0, 3, 0],
+                            [3, 1, 'A', 1, 3, 0, 2, 1, 1, 2, 0, 2],
+                            [3, 1, 1, 2, 1, 2, 1, 3, 1, 2, 1, 2],
+                            [3, 0, 0, 1, 2, 1, 1, 3, 1, 0, 2, 0],
+                            [3, 3, 3, 3, 1, 1, 2, 0, 3, 3, 3, 3]]
     if random.getrandbits(1) == 1:
-        grid_quarter_pattern.reverse()
+        grid_partial_pattern.reverse()
 
-    for x in range(math.floor(w / 2)):
-        for y in range(math.floor(h / 2)):
-            grid_template[x][y] = grid_quarter_pattern[math.floor(y / (h / 24)) % 12][math.floor(x / (w / 24)) % 12]
-    grid_quarter = randomize_grid(grid_template, math.floor(w / 2), math.floor(h / 2))
+    grid_template = [[0 for y in range(math.floor(h / y_portion))] for x in range(math.floor(w / x_portion))]
 
-    grid = [[False for y in range(h)] for x in range(w)]
-    grid = insert_grid(grid, grid_quarter, w, h, 0, 0)
-    grid = insert_grid(grid, grid_quarter, w, h, math.floor(w / 2), math.floor(h / 4))
-    grid = insert_grid(grid, grid_quarter, w, h, 0, math.floor(h / 2))
-    grid = insert_grid(grid, grid_quarter, w, h, math.floor(w / 2), math.floor((3 * h) / 4))
+    for x in range(math.floor(w / x_portion)):
+        for y in range(math.floor(h / y_portion)):
+            grid_template[x][y] = grid_partial_pattern[math.floor(y / (h / (y_portion * 12))) % 12][math.floor(x / (w / (x_portion * 12))) % 12]
+    grid_part = randomize_grid(grid_template, math.floor(w / x_portion), math.floor(h / y_portion))
+
+    grid = [[None for y in range(h)] for x in range(w)]
+    # grid = insert_grid(grid, grid_part, portion, w, h, 0, 0)
+    # grid = insert_grid(grid, grid_part, portion, w, h, math.floor(w / portion), math.floor(h / (portion * 2)))
+    # grid = insert_grid(grid, grid_part, portion, w, h, 0, math.floor(h / portion))
+    # grid = insert_grid(grid, grid_part, portion, w, h, math.floor(w / portion), math.floor((3 * h) / (portion * 2)))
+    for x in range(x_portion):
+        for y in range(y_portion):
+            # grid = insert_grid(grid, grid_part, portion, w, h, math.floor(w / portion) * x, math.floor((h * (y * portion + x)) / (portion * portion)))
+            grid = insert_grid(grid, grid_part, x_portion, y_portion, w, h, math.floor(w / x_portion) * x,
+                               math.floor((h / y_portion)) * y + math.floor((h / y_portion) * (x / 2)))
+    # grid = insert_grid(grid, grid_part, portion, w, h, math.floor(w / portion), math.floor(h / (portion * 2)))
+    # grid = insert_grid(grid, grid_part, portion, w, h, 0, math.floor(h / portion))
+    # grid = insert_grid(grid, grid_part, portion, w, h, math.floor(w / portion), math.floor((3 * h) / (portion * 2)))
     return grid
 
+
 def randomize_grid(grid_template, w, h):
-    grid = [[0 for y in range(h)] for x in range(w)]
+    grid = [[Tile(False) for y in range(h)] for x in range(w)]
+    grid_a = []
+    grid_b = []
     for x in range(w):
         for y in range(h):
-            if grid_template[x][y] == 2:
-                grid[x][y] = random.randrange(0, 100) < 90
+            if grid_template[x][y] == 'A':
+                grid_a.append([x, y])
+                grid[x][y].walkable = random.randrange(0, 100) < 65
+            elif grid_template[x][y] == 'B':
+                grid_b.append([x, y])
+                grid[x][y].walkable = random.randrange(0, 100) < 65
+            elif grid_template[x][y] == 2:
+                grid[x][y].walkable = random.randrange(0, 100) < 90
             elif grid_template[x][y] == 3:
-                grid[x][y] = random.randrange(0, 100) < 10
-            elif random.randrange(0, 100) < 25:
-                grid[x][y] = not grid_template[x][y]
+                grid[x][y].walkable = random.randrange(0, 100) < 10
+            elif random.randrange(0, 100) < 30:
+                grid[x][y].walkable = not grid_template[x][y]
             else:
-                grid[x][y] = grid_template[x][y]
+                grid[x][y].walkable = grid_template[x][y]
 
-    for _ in range(4):
-        grid_count = [[random.randrange(0, 100) < 45 for y in range(h)] for x in range(w)]
+    a_index = grid_a[random.randint(0, len(grid_a) - 1)]
+    b_index = grid_b[random.randint(0, len(grid_b) - 1)]
+
+    grid[a_index[0]][a_index[1]].hive = True
+    grid[a_index[0]][a_index[1]].walkable = True
+
+    grid[b_index[0]][b_index[1]].hive = True
+    grid[b_index[0]][b_index[1]].walkable = True
+
+    for _ in range(5):
+        grid_count = [[10 for y in range(h)] for x in range(w)]
         for x in range(w):
             for y in range(h):
-                count = -1 if grid[x][y] else 0;
+                if grid[x][y].hive:     # hive is always walkable
+                    continue
+                count = -1 if grid[x][y].walkable else 0;
                 for x_plus in [-1, 0, 1]:
                     for y_plus in [-1, 0, 1]:
-                        if x + x_plus >= 0 and y + y_plus >= 0 and x + x_plus < w and y + y_plus < h:
-                            count += 1 if grid[x + x_plus][y + y_plus] else 0
+                        if 0 <= x + x_plus < w and 0 <= y + y_plus < h:
+                            if grid[x + x_plus][y + y_plus].hive:
+                                count = 10
+                                break
+                            count += 1 if grid[x + x_plus][y + y_plus].walkable else 0
                         else:
                             if x + x_plus < 0:
-                                count += 1 if grid[w - 1][(y + y_plus - math.floor(h / 2)) % h] else 0
+                                if grid[w - 1][(y + y_plus - math.floor(h / 2)) % h].hive:
+                                    count = 10
+                                    break
+                                count += 1 if grid[w - 1][(y + y_plus - math.floor(h / 2)) % h].walkable else 0
+                            elif x + x_plus >= w:
+                                if grid[0][(y + y_plus - math.floor(h / 2)) % h].hive:
+                                    count = 10
+                                    break
+                                count += 1 if grid[0][(y + y_plus + math.floor(h / 2)) % h].walkable else 0
                             else:
-                                count += 1 if grid[0][(y + y_plus + math.floor(h / 2)) % h] else 0
+                                if grid[x + x_plus][(y + y_plus) % h].hive:
+                                    count = 10
+                                    break
+                                count += 1 if grid[x + x_plus][(y + y_plus) % h].walkable else 0
                 grid_count[x][y] = count
         for x in range(w):
             for y in range(h):
-                if grid[x][y]:
-                    grid[x][y] = grid_count[x][y] >= 4
+                if grid[x][y].walkable:
+                    grid[x][y].walkable = grid_count[x][y] >= 4
                 else:
-                    grid[x][y] = grid_count[x][y] >= 5
+                    grid[x][y].walkable = grid_count[x][y] >= 5
     return grid
+
 
 def get_bots():
     bot_path = './bots'
 
     bot_names = [f[0:-3] for f in listdir(bot_path) if isfile(join(bot_path, f))]
     bot_ais = [importlib.import_module('bots.' + f).AI(X_SIZE, Y_SIZE) for f in bot_names]
-    
+
     return [Bot(n, a) for n, a in zip(bot_names, bot_ais)]
+
 
 def main():
     pygame.init()
