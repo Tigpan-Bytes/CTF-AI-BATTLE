882fc9d867120e5c9e69311da69fb1ec83ec1ac3
BuildTools <unconfigured@null.spigotmc.org>
2019-12-10 18:03:47 -0600

    finished automation

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ class Game:
         self.world.tiles = self.set_hives(w, h, self.world.tiles)
         self.world.generate_neighbors()
         self.resize_board(math.floor(700 * max(X_SIZE / Y_SIZE, 1)) + 450, math.floor(700 * max(Y_SIZE / X_SIZE, 1)))
-        
+
         self.turn = 0
         self.death_position = len(self.bots)
         self.game_ended = False
@@ -156,15 +156,15 @@ class Game:
                     pygame.draw.polygon(self.screen, self.bots[i].colour,
                                         [(bee.position.x * self.cell_size + self.x_plus + 1,
                                           (
-                                                      Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + self.half_cell),
+                                                  Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + self.half_cell),
                                          (bee.position.x * self.cell_size + self.x_plus + self.half_cell,
                                           (Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + 1),
                                          (bee.position.x * self.cell_size + self.x_plus + self.cell_size - 1,
                                           (
-                                                      Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + self.half_cell),
+                                                  Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + self.half_cell),
                                          (bee.position.x * self.cell_size + self.x_plus + self.half_cell,
                                           (
-                                                      Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + self.cell_size - 1)])
+                                                  Y_SIZE - 1) * self.cell_size - bee.position.y * self.cell_size + self.cell_size - 1)])
 
             i = i + 1
 
@@ -944,9 +944,13 @@ def get_winner():
             return [game.rankings[len(game.rankings) - i - 1].team for i in range(len(game.rankings))]
     return None
 
-def quit_game():
-    pygame.quit()
-    sys.exit()
+
+def quit_game(force=False):
+    if force:
+        pygame.quit()
+    else:
+        sys.exit()
+
 
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
@@ -960,19 +964,14 @@ pygame.display.set_caption("CTF: Bee Swarm Battle")
 # creates the game
 game = Game(X_SIZE, Y_SIZE)
 
-def loop():
+while 1:
     game.update()
 
     pygame.display.update()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            quit_game()
+            pygame.quit()
+            sys.exit()
         if event.type == pygame.VIDEORESIZE:
             game.resize_board(event.w, event.h)
-
-
-if __name__ == '__main__':
-    while 1: 
-        loop()
-
 
